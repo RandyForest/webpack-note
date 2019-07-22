224c45f10b4a4646ef8a9f6ab532f2ac96412cad
RandyForest <randy7100@outlook.com>
2019-07-22 21:50:46 +0800

    忽略node_modules

---
+++ demo-conf/webpack.config.js
@@ -0,0 +1,14 @@
+let Path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'build.js',
+        path: Path.resolve('./build')
+    },
+    devServer: {},
+    module: {},
+    plugins: [],
+    mode: 'development',
+    resolve: {}
+}
